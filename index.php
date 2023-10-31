8b39cb6a8bba5d679a807d7b4fbf0e6c3021528a
Ulviquellovero <ulivi.samuel@volta.ts.it>
2023-10-31 18:48:42 +0100

    FInite cose, fare pulsante

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/index_style.css">
+        <meta charset="ISO-8859-1">
+    </head>
+
+    <body>
+        <div id="login_container">
+            <h2 id="login_header">Sign In</h2>
+            <form id="login_form" method="post" action="index.php">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <input id="login_btn" type="submit" name="login" value="Sign In">
+            </form>
+        </div>
+
+        <?php
+            if(isset($_POST['username']))
+            {
+                $username = $_POST['username'];
+                if(isset($_POST['password']))
+                {
+                    $password = $_POST['password'];
+                    $con = mysqli_connect("localhost", "root", "", "memo2");
+                    $sql = "SELECT * FROM tLogin tl WHERE tl.username = '".$username."' AND tl.password = '".$password."'";
+                    $res = mysqli_query($con, $sql);
+                    if(mysqli_num_rows($res) == 1)
+                    {
+                        header("Location: notes_list.php");
+                        mysqli_close($con);
+                        exit;
+                    }
+                    else
+                    {
+                        echo "<p id='no_log_text'>Incorrect username or password.<p>";
+                        mysqli_close($con);
+                    }
+                }
+            }
+        ?>
+    </body>
+</html>
---
+++ notes_list.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/notes_list_style.css">
+        <meta charset="ISO-8859-1">
+    </head>
+
+    <body>
+        <h1 id="title">Note</h1>
+        <?php
+            $con = mysqli_connect("localhost", "root", "", "memo2");
+            $sql = "SELECT titolo, testo FROM tmemo tm WHERE tm.titolo != '' OR tm.testo != '' ORDER BY titolo ASC, testo ASC";
+            $res = mysqli_query($con, $sql);
+            if(mysqli_num_rows($res) > 0)
+            {
+                while($row = mysqli_fetch_assoc($res))
+                {
+                    echo "<div class='tile'>";
+                    echo "<h2 class='tile_title'>".$row['titolo']."</h2>";
+                    if(strlen($row['testo']) < 20)
+                        echo "<p class='tile_text_piece'>".$row['testo']."</p>";
+                    else
+                        echo "<p class='tile_text_piece'>".substr($row['testo'], 0, 20)."...</p>";
+                    echo "</div>";
+                }
+            }
+        ?>
+        <button id="floating_button">+</button>
+    </body>
+</html>
